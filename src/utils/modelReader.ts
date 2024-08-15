787a566d57681c58c65015b36e230679168686ac
Dmitri <dmitri.nasonov.b@gmail.com>
2024-08-15 15:28:52 +0300

    Added non-existing in openrouter models for backwards compatibility

--- src/utils/modelReader.ts
+++ src/utils/modelReader.ts
@@ -43,6 +43,43 @@ export const loadModels = async (): Promise<{
   const modelCost: ModelCost = {};
   const modelTypes: { [key: string]: string } = {};
 
+  // Prepend specific models
+  const specificModels = [
+    {
+      id: 'gpt-4-0125-preview',
+      context_length: 128000,
+      pricing: {
+        "prompt": "0.00001",
+        "completion": "0.00003",
+        "image": "0.01445",
+        "request": "0"
+      },
+      type: 'text',
+    },
+    {
+      id: 'gpt-4-turbo-2024-04-09',
+      context_length: 128000,
+      pricing: {
+        "prompt": "0.00001",
+        "completion": "0.00003",
+        "image": "0.01445",
+        "request": "0"
+      },
+      type: 'text',
+    },
+  ];
+
+  specificModels.forEach((model) => {
+    modelOptions.push(model.id);
+    modelMaxToken[model.id] = model.context_length;
+    modelCost[model.id] = {
+      prompt: { price: parseFloat(model.pricing.prompt), unit: 1 },
+      completion: { price: parseFloat(model.pricing.completion), unit: 1 },
+      image: { price: parseFloat(model.pricing.image), unit: 1 },
+    };
+    modelTypes[model.id] = model.type;
+  });
+
   modelsJson.data.forEach((model) => {
     const modelId = model.id.split('/').pop() as string;
     modelOptions.push(modelId);
