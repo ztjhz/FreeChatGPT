a84988363779c26453d40e68826764071e042ef8
Dmitri Nasonov <dmitri.nasonov.b@gmail.com>
2024-09-13 16:14:37 +0100

    Added support for non-stream models (o1-)

--- src/components/StopGeneratingButton/StopGeneratingButton.tsx
+++ src/components/StopGeneratingButton/StopGeneratingButton.tsx
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import useStore from '@store/store';
+import { useTranslation } from 'react-i18next';
+import { modelStreamSupport } from '@constants/modelLoader';
 
 const StopGeneratingButton = () => {
+  const { t } = useTranslation();
   const setGenerating = useStore((state) => state.setGenerating);
   const generating = useStore((state) => state.generating);
+
+  const currentModel = useStore((state) =>
+    state.chats ? state.chats[state.currentChatIndex].config.model : ''
+  );
+  const handleGeneratingStop = () => {
+    if (modelStreamSupport[currentModel]) {
+      setGenerating(false);
+    } else {
+      const confirmMessage = t('stopNonStreamGenerationWarning');
+      if (window.confirm(confirmMessage)) {
+        setGenerating(false);
+      }
+    }
+  };
 
   return generating ? (
     <div
       className='absolute bottom-6 left-0 right-0 m-auto flex md:w-full md:m-auto gap-0 md:gap-2 justify-center'
-      onClick={() => setGenerating(false)}
+      onClick={() => handleGeneratingStop()}
     >
       <button
         className='btn relative btn-neutral border-0 md:border'
